c5e5b4dda9e5dddee63be03da94e4de50e706c86
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-08 17:01:30 +0600

    basic conce[

--- basicConcept.js
+++ basicConcept.js
@@ -1,1 +1,46 @@
-//
+// 1. write 3 variables (number, string, boolean
+
+var varNum =55;
+var varString = "hello World";
+var varBoolean = true;
+
+// console.log(varNum);
+// console.log(varString);
+// console.log(varBoolean);
+
+// 2. 2 variables using (let, const)
+
+let myAge=34;
+const myName = "Ferdous Alam Parag";
+// console.log(myAge);
+// console.log(myName);
+
+
+// 3. Simple Math Operations() +, -, *, /, %)
+
+/* var a =50;
+var b = 25;
+var mathOperation;
+mathOperation = a+b;
+console.log(mathOperation);
+mathOperation =a-b;
+console.log(mathOperation);
+mathOperation =a*b;
+console.log(mathOperation);
+mathOperation =a/b;
+console.log(mathOperation);
+mathOperation =a%b;
+console.log(mathOperation); */
+
+// 4. comparison ()
+
+var a =50;
+var b = 25;
+console.log(a==b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a!=b);
+
+// 5. two conditions. case-1: fulfill both conditions. case-2: fulfill at least one condition
+
+
